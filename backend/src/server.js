a078d8c01c101c2aaf7404bc44cca0710551c6e6
Kennedy <kennedybacelar@yahoo.com.br>
2020-03-10 23:50:35 +0100

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+const server = express();
+
+mongoose.connect('mongodb+srv://kennedy:boleirok10@cluster0-kqa2h.mongodb.net/test?retryWrites=true&w=majority',{
+   useNewUrlParser: true, useUnifiedTopology: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(8888);
+
